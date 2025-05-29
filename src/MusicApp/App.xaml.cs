dc7da2f920d4b90def3301f4c36e7992bcf6e676
Albert Akhmetov <akhmetov@live.com>
2025-05-29 16:50:21 +0500

    Add the application project

---
+++ src/MusicApp/App.xaml.cs
@@ -0,0 +1,92 @@
+/*  Copyright © 2025, Albert Akhmetov <akhmetov@live.com>   
+ *
+ *  This file is part of MusicApp.
+ *
+ *  MusicApp is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MusicApp is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MusicApp. If not, see <https://www.gnu.org/licenses/>.   
+ *
+ */
+ namespace MusicApp;
+
+using System;
+using System.Collections.Immutable;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.UI;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Windows.AppLifecycle;
+using WinRT.Interop;
+
+public partial class App : Application
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+
+        Start(_ =>
+        {
+            var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+            SynchronizationContext.SetSynchronizationContext(context);
+
+            instance = new App(args);
+        });
+
+        instance?.host?.StopAsync().Wait();
+        instance?.host?.Dispose();
+    }
+
+    private static App? instance;
+
+    private readonly ImmutableArray<string> arguments;
+    private IHost? host;
+    private MainWindow? mainWindow;
+
+    public App(string[] args)
+    {
+        this.arguments = ImmutableArray.Create(args);
+
+        InitializeComponent();
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        base.OnLaunched(args);
+
+        host = CreateHost();
+
+        mainWindow = host.Services.GetRequiredService<MainWindow>();
+        mainWindow.Closed += OnMainWindowClosed;
+        mainWindow.AppWindow.Show(true);
+
+        _ = host.RunAsync();
+    }
+
+    private void OnMainWindowClosed(object sender, WindowEventArgs args)
+    {
+        Exit();
+    }
+
+    private IHost CreateHost()
+    {
+        var builder = Host.CreateApplicationBuilder();
+
+        builder.Services.AddSingleton<MainWindow>();
+
+        return builder.Build();
+    }
+}
---
+++ src/MusicApp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿/*  Copyright © 2025, Albert Akhmetov <akhmetov@live.com>   
+ *
+ *  This file is part of MusicApp.
+ *
+ *  MusicApp is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MusicApp is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MusicApp. If not, see <https://www.gnu.org/licenses/>.   
+ *
+ */
+ namespace MusicApp;
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+
+public partial class MainWindow : Window
+{
+    public MainWindow(IServiceProvider serviceProvider)
+    {
+        this.InitializeComponent();
+    }
+}
